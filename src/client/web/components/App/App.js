f4a643bc2cd29e24e5fbf16dc80e061672f03c58
Priya Porwal <priya.spcres@gmail.com>
2020-02-23 00:00:44 +0530

    initial changes

--- src/client/web/components/App/App.js
+++ src/client/web/components/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, from client!</h1>;
+    return <h1>Hello, Welcome to client app!</h1>;
   }
 }
 
